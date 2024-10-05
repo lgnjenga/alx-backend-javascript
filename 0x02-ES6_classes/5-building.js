f1fdcde9a2bca521353ee0889d9c843bf51e5bbd
lgnjenga <lgnjenga@gmail.com>
2024-10-05 03:32:25 +0300

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate an abstract class');
-    }
-    this._sqft = sqft; // Store the square footage
+    this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Method to be overridden by subclasses
   evacuationWarningMessage() {
-    // This method can be empty but uses 'this' for compliance with ESLint
-    return 'This method must be overridden';
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
   }
 }
